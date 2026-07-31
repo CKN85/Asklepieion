d9da22be40764043a771458e0631b54a0be3829e
CKN85 <chill.kanin@gmail.com>
2026-08-01 01:12:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   // Using a custom domain at the root ("/") — if you're NOT using a custom
   // domain and are instead on username.github.io/asklepieion/, change this
   // to base: '/asklepieion/' (your repo name) or pages will load blank.
-  base: '/',
+  base: '/Asklepieion/',
 })
